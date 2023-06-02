cff5676a1b0c470035db990b10cada424aa38114
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-06-02 11:03:18 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-mysanthropi-hellodjango-cjw94tisg7x.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['relidev1992-django-todo-app.herokuapp.com']
 
 
 # Application definition
